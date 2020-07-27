31b5a152594b691da834fed7b78c12c9fbf637f5
Eugene Kim <eugkim1997@gmail.com>
2020-07-27 14:36:30 -0700

    added click functionality to buttons

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,24 +2,30 @@
 Model
 -Tic Tac Toe Board Data structure: 3 x 3 matrix/nested array
 */
-window.board = [['#', '#', '#'],
-                ['#', '#', '#'],
-                ['#', '#', '#']];
+var board = [['#', '#', '#'],
+             ['#', '#', '#'],
+             ['#', '#', '#']];
+var count = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+var move = 'O';
 
 var displayBoard = function() {
-  var count = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',]
   var countIndex = 0;
-  for (var i = 0; i < window.board.length; i++) {
-    for (var j = 0; j < window.board[i].length; j++) {
-      var current = document.getElementById(count[countIndex]);
-      var content = document.createTextNode(window.board[i][j]);
-      current.appendChild(content);
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      document.getElementById(count[countIndex]).innerHTML = board[i][j];
       countIndex++;
     }
   }
 }
 displayBoard();
+
 /*
 Controller
 -Methods that update the matrix and check for a winner
 */
+
+//
+var changeContent = function(i, j, content) {
+  board[i][j] = content;
+  displayBoard();
+}
