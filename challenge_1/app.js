5485398922c7f45b94316a2489821046cb3ac5de
Eugene Kim <eugkim1997@gmail.com>
2020-07-27 16:52:15 -0700

    added winner-checking

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,22 +5,77 @@ Model
 */
 
 //current contents of tictactoe board
-var blueprints = [['#', '#', '#'],
-                  ['#', '#', '#'],
-                  ['#', '#', '#']];
+var blueprint = [['1', '2', '3'],
+                  ['4', '5', '6'],
+                  ['7', '8', '9']];
 var count = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 var turn = 'X';
 
-//maintains elements(buttons) displayed on html board, reflects blueprints
+//maintains elements(buttons) displayed on html board, reflects blueprint
 var updateBoard = function() {
   var countIndex = 0;
-  for (var i = 0; i < blueprints.length; i++) {
-    for (var j = 0; j < blueprints[i].length; j++) {
-      document.getElementById(count[countIndex]).innerHTML = blueprints[i][j];
+  for (var i = 0; i < blueprint.length; i++) {
+    for (var j = 0; j < blueprint[i].length; j++) {
+      document.getElementById(count[countIndex]).innerHTML = blueprint[i][j];
       countIndex++;
     }
   }
 };
+
+//if blueprint[row][i] === turn
+
+//checks current board for winner
+var winnerCheck = function() {
+  var result = 'tie';
+  result = verticalChecks();
+  if (result === 'tie') {
+    result = horizontalChecks();
+  }
+  if (result === 'tie') {
+    result = diagonalChecks();
+  }
+  if (result !== 'tie') {
+    console.log('Winner: ', result);
+  }
+};
+
+//checks for a vertical winner
+var verticalChecks = function(result) {
+  var result = 'tie';
+  if (blueprint[0][0] === blueprint[1][0] && blueprint[0][0] === blueprint[2][0] && blueprint[1][0] === blueprint[2][0]) {
+    result = blueprint[0][0];
+  } else if (blueprint[0][1] === blueprint[1][1] && blueprint[0][1] === blueprint[2][1] && blueprint[1][1] === blueprint[2][1]) {
+    result = blueprint[0][1];
+  } else if (blueprint[0][2] === blueprint[1][2] && blueprint[0][2] === blueprint[2][2] && blueprint[1][2] === blueprint[2][2]) {
+    result = blueprint[0][2];
+  }
+  return result;
+};
+
+//checks for a horizontal winner
+var horizontalChecks = function(result) {
+  var result = 'tie';
+  if (blueprint[0][0] === blueprint[0][1] && blueprint[0][0] === blueprint[0][2] && blueprint[0][1] === blueprint[0][2]) {
+    result = blueprint[0][0];
+  } else if (blueprint[1][0] === blueprint[1][1] && blueprint[1][0] === blueprint[1][2] && blueprint[1][1] === blueprint[1][2]) {
+    result = blueprint[1][0];
+  } else if (blueprint[2][0] === blueprint[2][1] && blueprint[2][0] === blueprint[2][2] && blueprint[2][1] === blueprint[2][2]) {
+    result = blueprint[2][0];
+  }
+  return result;
+};
+
+//checks for a diagonal winner
+var diagonalChecks = function(result) {
+  var result = 'tie';
+  if (blueprint[0][0] === blueprint[1][1] && blueprint[0][0] === blueprint[2][2] && blueprint[1][1] === blueprint[2][2]) {
+    result = blueprint[0][0];
+  } else if (blueprint[0][2] === blueprint[1][1] && blueprint[0][2] === blueprint[2][0] && blueprint[1][1] === blueprint[2][0]) {
+    result = blueprint[0][2];
+  }
+  return result;
+};
+
 //starts app
 updateBoard();
 
@@ -30,10 +85,11 @@ Controller
 */
 
 //
-var changeContent = function(i, j, content) {
-  blueprints[i][j] = content;
+var changeContent = function(row, column, content) {
+  blueprint[row][column] = content;
   updateBoard();
   switchTurn();
+  winnerCheck();
 };
 
 var switchTurn = function() {
