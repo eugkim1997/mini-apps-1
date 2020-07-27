cd91c20a5dc7a01862934f3cbb833171c30d3a05
Eugene Kim <eugkim1997@gmail.com>
2020-07-27 12:31:24 -0700

    Implemented MVC architecture

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,17 @@
+/*
+Model
+-Tic Tac Toe Board Data structure: 3 x 3 matrix/nested array
+*/
+window.board = [['#', '#', '#'],
+                ['#', '#', '#'],
+                ['#', '#', '#']];
 
+document.body.onload = displayBoard;
 
+var displayBoard = function() {
+}
+
+/*
+Controller
+-Methods that update the matrix and check for a winner
+*/
